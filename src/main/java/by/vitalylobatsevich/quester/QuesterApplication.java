87eff4a21506c3463759e5bd05fb01bcb5a5599a
vit <vit@MrComp>
2022-07-14 19:30:05 +0300

    Project base

---
+++ src/main/java/by/vitalylobatsevich/quester/QuesterApplication.java
@@ -0,0 +1,13 @@
+package by.vitalylobatsevich.quester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuesterApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(QuesterApplication.class, args);
+    }
+
+}
---
+++ src/main/java/by/vitalylobatsevich/quester/configuration/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package by.vitalylobatsevich.quester.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
+        return http
+                .authorizeRequests(
+                        customizer -> customizer
+                                .antMatchers(
+                                        HttpMethod.GET,
+                                        "/",
+                                        "/css/**",
+                                        "/js/**",
+                                        "/img/**",
+                                        "/favicon.ico"
+                                ).permitAll()
+                                .anyRequest().authenticated()
+                )
+                .build();
+    }
+
+}
